fa49dec10a8bbb611dad6f02a203d36f41d5578d
cosmiclatte <129236813+coslatte@users.noreply.github.com>
2026-08-14 17:44:35 -0400

    file header first steps

--- readme.md
+++ readme.md
@@ -7,3 +7,7 @@ No AI agents coding my stuff, just Q&A and critical thinking learning and taking
 kotlin so far looks really charming when it matters to low lever coding. despite java morbose sintaxis, this stuffs let's you be the same time verbose and clear, with quite cool modern syntax sauce and structures.
 
 just learned the differences between java and kotlin when it comes to _enums_ kind of structure, and it's really cool: we use something called `companion object`'s, and they work like such but in a really different way and with better performance that enums are defined in Java.
+
+## references
+
+https://en.wikipedia.org/wiki/WAV
--- src/main/kotlin/Header.kt
+++ src/main/kotlin/Header.kt
@@ -1,9 +1,36 @@
 class Header {
 
+    /**
+     * this structure describe the ID's the header chunk constants needs by format
+     */
     @JvmInline
-    value class ChunkID(val chunkID: Int) {
+    value class ChunkID(val value: UInt) {
         companion object {
-            val RIFF: Int = "RIFF".toInt()
+            val RIFF = ChunkID(0x52494646u) // "RIFF"
+            val WAVE = ChunkID(0x57415645u) // "WAVE"
+            val FMT = ChunkID(0x666D7420u) // "fmt "
+            val DATA = ChunkID(0x64617461u) // "data"
         }
     }
+
+    data class WavHeader(
+        // [Master RIFF chunk]
+        val fileTypeBlocID: ChunkID = ChunkID.RIFF,
+        val fileSize: UInt,
+        val fileFormatBlocID: ChunkID = ChunkID.WAVE,
+
+        // [Data «format» chunk]
+        val formatBlocID: ChunkID = ChunkID.FMT,
+        val blocSize: UInt,
+        val audioFormat: UShort,
+        val nbrChannels: UShort,
+        val frequency: UInt,
+        val bytePerSec: UInt,
+        val bytePerBloc: UShort,
+        val bitsPerSample: UShort,
+
+        // [Data chunk]
+        val dataBlocID: ChunkID = ChunkID.DATA,
+        val dataSize: UInt,
+    )
 }
