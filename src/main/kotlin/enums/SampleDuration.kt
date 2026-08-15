0b5ebaaa9b3dbd582c6ca964ddb2eec1780bb62e
cosmiclatte <129236813+coslatte@users.noreply.github.com>
2026-08-14 20:45:54 -0400

    help on sample duration for header information

---
+++ src/main/kotlin/enums/SampleDuration.kt
@@ -0,0 +1,31 @@
+package enums
+
+import WavHeader
+
+enum class SampleDuration(val seconds: Long) {
+    _1SEC(1L),
+    _3SEC(3L),
+    _5SEC(5L),
+    _10SEC(10L),
+    _30SEC(30L),
+    _1MIN(60L),
+    _3MIN(180L);
+
+    /**
+     * dataSize = duration * sampleRate * channels bitDepth * bytesPerSample
+     */
+    fun toDataSize(
+        sampleRate: SampleRate,
+        channels: ChannelCount,
+        bitDepth: BitDepth
+    ): UInt {
+        val bytesPerSample = bitDepth.bits.toLong() / 8L
+        val bytesPerSecond = sampleRate.hz.toLong() * channels.value.toLong() * bytesPerSample
+
+        return (seconds * bytesPerSecond).toUInt()
+    }
+
+    fun toDataSize(header: WavHeader): UInt {
+        return (seconds * header.byteRate.toLong()).toUInt()
+    }
+}
